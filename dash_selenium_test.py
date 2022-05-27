0c3a3e0907ef521d8d551df95fa5ed5276b4f08e
Jenna Dodge <94572628+jennadodge@users.noreply.github.com>
2022-05-26 20:08:47 -0600

    added server=app.server line

--- dash_selenium_test.py
+++ dash_selenium_test.py
@@ -26,7 +26,7 @@ app = Dash(__name__,
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]
                 )
-# server=app.server
+server=app.server
 
 # Read data
 df = pd.read_csv("census_data_for_graphs.csv", dtype={"zip":str,"fips":str})

77cee5a0ccb13d1a78f4ba53616cdbc188054b63
Jenna Dodge <94572628+jennadodge@users.noreply.github.com>
2022-05-27 09:57:00 -0600

    output of main utlity

--- dash_selenium_test.py
+++ dash_selenium_test.py
@@ -91,7 +91,7 @@ def confirm_zip(zip_code):
 
 @app.callback(
     Output('total_cont', 'children'),
-    Output('total_scrape_time','children'),
+    # Output('total_scrape_time','children'),
     Input('zip_input', 'value')
 )
 def scrape_ewg(zip_code):  # function arguments come from the component property of the Input
@@ -107,144 +107,147 @@ def scrape_ewg(zip_code):  # function arguments come from the component property
     #Build a list of the utilities to visit and scrape data from EWG
     utility_list = utilities_df['Utility name'].to_list()
 
-    start_time = time.time()
+    main_utility = table[0]["Utility name"][0]
+    output = "The main utility in this zip is {}".format(main_utility)
 
-    #Begin Scrape
-    # driver = webdriver.Chrome()
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    # start_time = time.time()
 
-    contaminant_list = []
-    for utility in utility_list:
-        try:
-            driver.get("https://www.ewg.org/tapwater/advanced-search.php")
-            time.sleep(1)
-            utility_input = driver.find_element(By.XPATH, '/html/body/div[3]/main/div/section[1]/form/input[1]')
-            utility_input.clear()
-            utility_input.send_keys(utility)
-            go_btn = driver.find_element(By.XPATH, '/html/body/div[3]/main/div/section[1]/form/input[3]')
-            go_btn.click()
-            time.sleep(1)
-            utility = driver.find_element(By.XPATH, '/html/body/div[3]/main/figure/table/tbody/tr/td[1]/a')
-            utility.click()
+    # #Begin Scrape
+    # # driver = webdriver.Chrome()
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
+    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-            page_source = driver.page_source
+    # contaminant_list = []
+    # for utility in utility_list:
+    #     try:
+    #         driver.get("https://www.ewg.org/tapwater/advanced-search.php")
+    #         time.sleep(1)
+    #         utility_input = driver.find_element(By.XPATH, '/html/body/div[3]/main/div/section[1]/form/input[1]')
+    #         utility_input.clear()
+    #         utility_input.send_keys(utility)
+    #         go_btn = driver.find_element(By.XPATH, '/html/body/div[3]/main/div/section[1]/form/input[3]')
+    #         go_btn.click()
+    #         time.sleep(1)
+    #         utility = driver.find_element(By.XPATH, '/html/body/div[3]/main/figure/table/tbody/tr/td[1]/a')
+    #         utility.click()
 
-            html_soup = soup(page_source, 'html.parser')
+    #         page_source = driver.page_source
 
-            #Get the name of the Water Utility
-            Utility = html_soup.find('h1').text
+    #         html_soup = soup(page_source, 'html.parser')
 
-            #get the html data we need
-            data_box = html_soup.find_all('div', class_='contaminant-name')
+    #         #Get the name of the Water Utility
+    #         Utility = html_soup.find('h1').text
 
-            for i in range(len(data_box)):
-                data = data_box[i].find_all('span')
-                data_measure = []
-                d = {
-                    'Utility' : Utility,
-                    'Contaminant': '', 
-                    'Utility Measuremnt':'', 
-                    'EWG HEALTH GUIDELINE': '',
-                    'Legal Limit':'' 
-                }
+    #         #get the html data we need
+    #         data_box = html_soup.find_all('div', class_='contaminant-name')
 
-                contaminant_name = data_box[i].find('h3')
-                d['Contaminant'] = contaminant_name
+    #         for i in range(len(data_box)):
+    #             data = data_box[i].find_all('span')
+    #             data_measure = []
+    #             d = {
+    #                 'Utility' : Utility,
+    #                 'Contaminant': '', 
+    #                 'Utility Measuremnt':'', 
+    #                 'EWG HEALTH GUIDELINE': '',
+    #                 'Legal Limit':'' 
+    #             }
 
-                for j in range(len(data)):
-                    measurement = data[j].text
-                    #print(measurement)
-                    data_measure.append(measurement)
-                    #print(data_measure)
+    #             contaminant_name = data_box[i].find('h3')
+    #             d['Contaminant'] = contaminant_name
 
-                # try:
-                #     d['Utility Measuremnt'] = data_measure[data_measure.index('THIS UTILITY')+1]
-                # except ValueError:
-                #     print("A value error arose")
-                # except:
-                #     print("Something else went wrong")
-                # try:
-                #     d['EWG HEALTH GUIDELINE'] = data_measure[data_measure.index('EWG HEALTH GUIDELINE')+1]
-                # except ValueError:
-                #     print("A value error arose")
-                # except:
-                #     print("Something else went wrong") 
-                # try:
-                #     d['Legal Limit'] = data_measure[data_measure.index('LEGAL LIMIT')+1]
-                # except ValueError:
-                #     print("A value error arose")
-                # except:
-                #     print("Something else went wrong") 
+    #             for j in range(len(data)):
+    #                 measurement = data[j].text
+    #                 #print(measurement)
+    #                 data_measure.append(measurement)
+    #                 #print(data_measure)
 
-                contaminant_list.append(d)
-        except:
-            pass
-    # print('Done Scraping, moving on to dataset cleaning and file writing')
-    #Construct a dataframe from the results
-    scraped_df = pd.DataFrame(contaminant_list)
+    #             # try:
+    #             #     d['Utility Measuremnt'] = data_measure[data_measure.index('THIS UTILITY')+1]
+    #             # except ValueError:
+    #             #     print("A value error arose")
+    #             # except:
+    #             #     print("Something else went wrong")
+    #             # try:
+    #             #     d['EWG HEALTH GUIDELINE'] = data_measure[data_measure.index('EWG HEALTH GUIDELINE')+1]
+    #             # except ValueError:
+    #             #     print("A value error arose")
+    #             # except:
+    #             #     print("Something else went wrong") 
+    #             # try:
+    #             #     d['Legal Limit'] = data_measure[data_measure.index('LEGAL LIMIT')+1]
+    #             # except ValueError:
+    #             #     print("A value error arose")
+    #             # except:
+    #             #     print("Something else went wrong") 
 
-    #Define a function to strip the h3 tages from the contaminants (relict from scraping)
-    import re
-    CLEANR = re.compile('<.*?>') 
+    #             contaminant_list.append(d)
+    #     except:
+    #         pass
+    # # print('Done Scraping, moving on to dataset cleaning and file writing')
+    # #Construct a dataframe from the results
+    # scraped_df = pd.DataFrame(contaminant_list)
 
-    def cleanhtml(raw_html):
-        cleantext = re.sub(CLEANR, '', raw_html)
-        return cleantext
+    # #Define a function to strip the h3 tages from the contaminants (relict from scraping)
+    # import re
+    # CLEANR = re.compile('<.*?>') 
 
-    scraped_df = scraped_df.astype({"Contaminant": str})
+    # def cleanhtml(raw_html):
+    #     cleantext = re.sub(CLEANR, '', raw_html)
+    #     return cleantext
 
-    scraped_df.Contaminant = scraped_df.Contaminant.apply(lambda x: cleanhtml(x))
+    # scraped_df = scraped_df.astype({"Contaminant": str})
 
-    # #Output the resulting dataframe to a csv file
-    # state_path_contaminant_output = os.path.join(path,'contaminants.csv')
-    # scraped_df.to_csv(state_path_contaminant_output, index=False)
+    # scraped_df.Contaminant = scraped_df.Contaminant.apply(lambda x: cleanhtml(x))
 
-    finish_time = time.time()
+    # # #Output the resulting dataframe to a csv file
+    # # state_path_contaminant_output = os.path.join(path,'contaminants.csv')
+    # # scraped_df.to_csv(state_path_contaminant_output, index=False)
 
-    total_time = (finish_time - start_time)/60
+    # finish_time = time.time()
 
-    # print(f'The process finished in finished in {total_time} minutes')
+    # total_time = (finish_time - start_time)/60
 
-    df = scraped_df.copy()
-    df =df.replace(r'^\s*$', np.nan, regex=True)
-    df = df.dropna()
-    df3 = df.copy()
-    #Split of the contaminant measurements and units and people served
-    df3['Units'] = df3['Utility Measuremnt'].apply(lambda x: x.split()[-1])
-    df3['Utility Measuremnt'] = df3['Utility Measuremnt'].apply(lambda x: x.split()[0])
-    df3['EWG HEALTH GUIDELINE'] = df3['EWG HEALTH GUIDELINE'].apply(lambda x: x.split()[0])
-    df3['Legal Limit']=df3['Legal Limit'].apply(lambda x: '0 units' if pd.isnull(x) else x)
-    df3['Legal Limit'] = df3['Legal Limit'].apply(lambda x: x.split()[0])
-    df3.replace(',','', regex=True, inplace=True)
-    #Change the datatype of the measurements and EWG Guidlines to numeric values
-    df3['Utility Measuremnt'] = pd.to_numeric(df3['Utility Measuremnt'])
-    df3['EWG HEALTH GUIDELINE'] = pd.to_numeric(df3['EWG HEALTH GUIDELINE'])
-    #Define the Contaminant Factor (how many times larger is the utility measurement than EWG guideline)
-    df3['Contaminant_Factor'] = df3['Utility Measuremnt']/df3['EWG HEALTH GUIDELINE']
-    utilities_grouped_df1 = df3.groupby('Utility').count()['Contaminant_Factor']
-    utilities_grouped_df2 = df3.groupby('Utility').sum()['Contaminant_Factor']
-    Sum_ContaminantFactor = utilities_grouped_df2.sum()
-    Avg_Contaminant_Factor = utilities_grouped_df2.mean()
-    Num_Contaminants = utilities_grouped_df1.sum()
-    #Output to be joined with census data for racial, ethnic, and income indices
-    data = {
-        'Zip' : zip_code,
-        'Sum_ContaminantFactor': Sum_ContaminantFactor, 
-        'Avg_Contaminant_Factor':Avg_Contaminant_Factor, 
-        'Num_Contaminants' : Num_Contaminants
-    }
+    # # print(f'The process finished in finished in {total_time} minutes')
 
-    df_final = pd.DataFrame(data, index=[0])
-    cont_num = df_final.loc[0,'Num_Contaminants']
-    total_contaminants = "The total contaminants found in your area is: {}".format(cont_num)
-    scrape_time = "The time it took to gather this data was: {}".format(total_time)
+    # df = scraped_df.copy()
+    # df =df.replace(r'^\s*$', np.nan, regex=True)
+    # df = df.dropna()
+    # df3 = df.copy()
+    # #Split of the contaminant measurements and units and people served
+    # df3['Units'] = df3['Utility Measuremnt'].apply(lambda x: x.split()[-1])
+    # df3['Utility Measuremnt'] = df3['Utility Measuremnt'].apply(lambda x: x.split()[0])
+    # df3['EWG HEALTH GUIDELINE'] = df3['EWG HEALTH GUIDELINE'].apply(lambda x: x.split()[0])
+    # df3['Legal Limit']=df3['Legal Limit'].apply(lambda x: '0 units' if pd.isnull(x) else x)
+    # df3['Legal Limit'] = df3['Legal Limit'].apply(lambda x: x.split()[0])
+    # df3.replace(',','', regex=True, inplace=True)
+    # #Change the datatype of the measurements and EWG Guidlines to numeric values
+    # df3['Utility Measuremnt'] = pd.to_numeric(df3['Utility Measuremnt'])
+    # df3['EWG HEALTH GUIDELINE'] = pd.to_numeric(df3['EWG HEALTH GUIDELINE'])
+    # #Define the Contaminant Factor (how many times larger is the utility measurement than EWG guideline)
+    # df3['Contaminant_Factor'] = df3['Utility Measuremnt']/df3['EWG HEALTH GUIDELINE']
+    # utilities_grouped_df1 = df3.groupby('Utility').count()['Contaminant_Factor']
+    # utilities_grouped_df2 = df3.groupby('Utility').sum()['Contaminant_Factor']
+    # Sum_ContaminantFactor = utilities_grouped_df2.sum()
+    # Avg_Contaminant_Factor = utilities_grouped_df2.mean()
+    # Num_Contaminants = utilities_grouped_df1.sum()
+    # #Output to be joined with census data for racial, ethnic, and income indices
+    # data = {
+    #     'Zip' : zip_code,
+    #     'Sum_ContaminantFactor': Sum_ContaminantFactor, 
+    #     'Avg_Contaminant_Factor':Avg_Contaminant_Factor, 
+    #     'Num_Contaminants' : Num_Contaminants
+    # }
 
-    return total_contaminants, scrape_time
+    # df_final = pd.DataFrame(data, index=[0])
+    # cont_num = df_final.loc[0,'Num_Contaminants']
+    # total_contaminants = "The total contaminants found in your area is: {}".format(cont_num)
+    # scrape_time = "The time it took to gather this data was: {}".format(total_time)
+
+    return output
 
     # # https://plotly.com/python/choropleth-maps/
     # fig = px.choropleth(
